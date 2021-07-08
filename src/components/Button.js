36356450c8dd0e496934241a33da61088cdcf715
xi1494-jaswantdhayal <jaswant.dhayal@xebia.com>
2021-07-08 16:59:23 +0530

    add image and location plugin

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import { TouchableOpacity, StyleSheet, Text } from "react-native";
-import COLORS from "../utils/colors";
+import React, {Component} from 'react';
+import {TouchableOpacity, StyleSheet, Text} from 'react-native';
+import COLORS from '../utils/colors';
 
 export class Button extends Component {
   render() {
-    const { title = "Button", onPress = () => {} } = this.props;
+    const {title = 'Button', onPress = () => {}} = this.props;
     return (
       <TouchableOpacity style={styles.main} onPress={onPress}>
         <Text style={styles.text}>{title}</Text>
@@ -15,16 +15,16 @@ export class Button extends Component {
 
 const styles = StyleSheet.create({
   main: {
-    justifyContent: "center",
-    alignContent: "center",
-    width: "100%",
+    justifyContent: 'center',
+    alignContent: 'center',
+    width: '100%',
     backgroundColor: COLORS.primary,
     padding: 10,
     marginVertical: 20,
   },
   text: {
     color: COLORS.white,
-    textAlign: "center",
+    textAlign: 'center',
     fontSize: 17,
   },
 });
--- src/screens/SubPage.js
+++ src/screens/SubPage.js
@@ -1,21 +1,116 @@
-import React, { Component } from "react";
-import { SafeAreaView, Text, StyleSheet } from "react-native";
-import STYLES from "../utils/styles";
+import React, {Component} from 'react';
+import {
+  ScrollView,
+  Text,
+  StyleSheet,
+  Image,
+  View,
+  Dimensions,
+  Linking,
+} from 'react-native';
+import STYLES from '../utils/styles';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import {Button} from '../components';
+import ImagePicker from 'react-native-image-crop-picker';
+import RNLocation from 'react-native-location';
 
+const {width} = Dimensions.get('screen');
 export default class SubPage extends Component {
+  state = {
+    images: [],
+  };
+
+  pickImage = () => {
+    ImagePicker.openPicker({
+      multiple: true,
+    })
+      .then(images => {
+        console.log(images);
+        this.setState({
+          images,
+        });
+      })
+      .catch(error => {
+        console.log(error.code);
+        if (error.code !== 'E_PICKER_CANCELLED') Linking.openSettings();
+      });
+
+    // // launchImageLibrary(
+    // //   {
+    // //     selectionLimit: 5,
+    // //     saveToPhotos: true,
+    // //   },
+    // //   data => {
+    // //     console.log('data => ', data);
+    // //     if (data.didCancel) alert('User Cancelled');
+    // //     else {
+    // //       this.setState({
+    // //         images: data.assets || [],
+    // //       });
+    // //     }
+    // //   },
+    // );
+  };
+
+  getLocation = () => {
+    RNLocation.requestPermission({
+      ios: 'whenInUse',
+      android: {
+        detail: 'coarse',
+      },
+    })
+      .then(granted => {
+        console.log('granted => ', granted);
+
+        if (granted)
+          RNLocation.getLatestLocation().then(location => {
+            console.log('location => ', location);
+            this.setState({
+              location,
+            });
+          });
+      })
+      .catch(error => {
+        console.log('error => ', error);
+      });
+  };
+
   render() {
+    const {images, location} = this.state;
     return (
-      <SafeAreaView style={[STYLES.main, styles.main]}>
-        <Text>This is Sub page</Text>
-      </SafeAreaView>
+      <ScrollView style={[STYLES.main, styles.main]}>
+        <Button title="Pick Image" onPress={this.pickImage} />
+        <View style={styles.imageUpper}>
+          {images.map((image, index) => {
+            return (
+              <Image
+                key={index}
+                style={styles.image}
+                source={{uri: image.sourceURL}}
+              />
+            );
+          })}
+        </View>
+        <Button title="Get My Location" onPress={this.getLocation} />
+        {location && <Text>{JSON.stringify(location)}</Text>}
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   main: {
-    justifyContent: "center",
-    alignContent: "center",
-    margin: 30,
+    alignContent: 'center',
+    margin: 20,
+  },
+  image: {
+    height: (width - 60) / 2,
+    width: (width - 60) / 2,
+    margin: 5,
+  },
+  imageUpper: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
 });
